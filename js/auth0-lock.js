a9819698da7e0adfbbfdb123427928df2d8b8306
Kimberly Nguy <kimnguyy@gmail.com>
2024-02-13 12:19:21 -0800

    Imported some scripts

--- js/auth0-lock.js
+++ js/auth0-lock.js
@@ -1,62 +1,64 @@
-const clientId = "WouQOQULCNjzNGqdUuIdcy0kgz4up7Nb"
-const domain = "dev-o64lsn21gx2ybdkn.us.auth0.com"
-const redirectUrl = `https://supreeth-ngpc-dev.platform9.horse/ui`
+// const clientId = "WouQOQULCNjzNGqdUuIdcy0kgz4up7Nb"
+// const domain = "dev-o64lsn21gx2ybdkn.us.auth0.com"
+// const redirectUrl = `https://supreeth-ngpc-dev.platform9.horse/ui`
 
-const options = {
-  auth: {
-    redirectUrl,
-    responseType: "token",
-  },
-  theme: {
-    logo: "https://spot.rackspace.com/static-files/spot-logo-transparent.png",
-    primaryColor: "#c91508",
-  },
-  languageDictionary: {
-    emailInputPlaceholder: "Email",
-    passwordInputPlaceholder: "Password",
-    forgotPasswordAction: "Forgot Password?",
-    title: "",
-  },
-  usernameStyle: "email",
-  allowShowPassword: true,
-}
+// const options = {
+//   auth: {
+//     redirectUrl,
+//     responseType: "token",
+//   },
+//   theme: {
+//     logo: "https://spot.rackspace.com/static-files/spot-logo-transparent.png",
+//     primaryColor: "#c91508",
+//   },
+//   languageDictionary: {
+//     emailInputPlaceholder: "Email",
+//     passwordInputPlaceholder: "Password",
+//     forgotPasswordAction: "Forgot Password?",
+//     title: "",
+//   },
+//   usernameStyle: "email",
+//   allowShowPassword: true,
+// }
 
-const lock = new Auth0Lock(clientId, domain, options)
+// const lock = new Auth0Lock(clientId, domain, options)
 
-// const auth0 = new Auth0Client({
+// // const auth0 = new Auth0Client({
+// //   domain,
+// //   clientId,
+// // })
+
+// var auth0Client = null
+
+// createAuth0Client({
 //   domain,
 //   clientId,
+// }).then((auth0) => {
+//   auth0Client = auth0
 // })
 
-var auth0Client = null
+// // Adjust the height of the Auth0 Lock widget
+// lock.on("show", function () {
+//   // Wait for the next tick to ensure the DOM has updated
+//   setTimeout(function () {
+//     const lockWidget = document.querySelector(".auth0-lock-widget")
+//     if (lockWidget) {
+//       lockWidget.style.height = "max-content"
+//       lockWidget.style.width = "350px"
+//     }
+//   }, 1)
+// })
 
-createAuth0Client({
-  domain,
-  clientId,
-}).then((auth0) => {
-  auth0Client = auth0
-})
+// lock.on("authenticated", function (authResult) {
+//   lock.getUserInfo(authResult.accessToken, function (error, profile) {
+//     if (error) {
+//       console.log("error")
+//       return
+//     }
 
-// Adjust the height of the Auth0 Lock widget
-lock.on("show", function () {
-  // Wait for the next tick to ensure the DOM has updated
-  setTimeout(function () {
-    const lockWidget = document.querySelector(".auth0-lock-widget")
-    if (lockWidget) {
-      lockWidget.style.height = "max-content"
-      lockWidget.style.width = "350px"
-    }
-  }, 1)
-})
+//     // Save the token and profile information in local storage or a global variable
+//     console.log("authenticated lock on")
+//   })
+// })
 
-lock.on("authenticated", function (authResult) {
-  lock.getUserInfo(authResult.accessToken, function (error, profile) {
-    if (error) {
-      console.log("error")
-      return
-    }
-
-    // Save the token and profile information in local storage or a global variable
-    console.log("authenticated lock on")
-  })
-})
+console.log("auth0 js file")
